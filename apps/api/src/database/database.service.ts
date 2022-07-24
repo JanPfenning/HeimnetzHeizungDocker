c418605bdec0916810453d6d3f2cab9e68a3bcb0
ppfenning92 <patrick.pfenning.92@gmail.com>
2022-07-24 13:38:07 +0200

    cleanup api

--- apps/api/src/database/database.service.ts
+++ apps/api/src/database/database.service.ts
@@ -1,46 +1,47 @@
 import { Injectable } from '@nestjs/common';
-import {Connection} from "mariadb/callback";
+import { Connection } from "mariadb/callback";
 
 @Injectable()
 export class DatabaseService {
 
-    async healthCheckDatabase(): Promise<boolean> {
-        const x = await this.prepareConnection()
-        return !!x.info.status; //TODO maybe a different variable has to be used
-    }
+  async healthCheckDatabase(): Promise<boolean> {
+    const x = await this.prepareConnection()
+    return !!x.info.status; //TODO maybe a different variable has to be used
+  }
 
-    storeTemperature(celsius: string) {
-        this.query(`insert into temperature (celsius) values (${celsius})`)
-    }
+  storeTemperature(celsius: string) {
+    this.query(`insert into temperature (celsius) values (${celsius})`)
+  }
 
-    private async query(query: string) {
-        const conn = await this.prepareConnection();
-        conn.query(query,
-            (err, rows) => {
-                conn.end();
-                return rows;
-            }
-        );
-    }
+  private async query(query: string) {
+    const conn = await this.prepareConnection();
+    conn.query(query,
+      (err, rows) => {
+        conn.end();
+        return rows;
+      }
+    );
+  }
 
-    private async prepareConnection(): Promise<Connection> {
-        //https://hub.docker.com/_/mariadb
-        //https://mariadb.com/kb/en/nodejs-connection-options/
-        const mariadb = require('mariadb/callback');
-        const conn = mariadb.createConnection({
-            host: process.env.DATABASE_IP,
-            port: process.env.DATABASE_PORT,
-            ssl: false,
-            user: "root",//process.env.DATABASE_USER,
-            password: process.env.DATABASE_ROOT_HASH,//process.env.DATABASE_PASS_HASH,
-            database:'iot'
-        })
-        await conn.connect(data => {
-
-        },
-        err => {
-            if(err) return null
-        })
-        return conn;
+  private async prepareConnection(): Promise<Connection> {
+    //https://hub.docker.com/_/mariadb
+    //https://mariadb.com/kb/en/nodejs-connection-options/
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    const mariadb = require('mariadb/callback');
+    const conn = mariadb.createConnection({
+      host: process.env['DATABASE_IP'],
+      port: process.env['DATABASE_PORT'],
+      ssl: false,
+      user: "root",//process.env['DATABASE_USER'],
+      password: process.env['DATABASE_ROOT_HASH'],//process.env['DATABASE_PASS_HASH'],
+      database: 'iot'
+    })
+    try {
+      await conn.connect();
+    } catch (e) {
+      console.error(e)
+      return null
     }
+    return conn;
+  }
 }
--- apps/api/src/fronius-photovoltaik/fronius-photovoltaik.controller.ts
+++ apps/api/src/fronius-photovoltaik/fronius-photovoltaik.controller.ts
@@ -1,35 +1,35 @@
-import {Controller, Get} from '@nestjs/common';
-import {FroniusPhotovoltaikService} from "./fronius-photovoltaik.service";
+import { Controller, Get } from '@nestjs/common';
+import { FroniusPhotovoltaikService } from "./fronius-photovoltaik.service";
 
 @Controller('fronius-photovoltaik')
 export class FroniusPhotovoltaikController {
 
-    constructor(private readonly froniusService: FroniusPhotovoltaikService) {
-        this.froniusService.logGeneralInfo()
-    }
+  constructor(private readonly froniusService: FroniusPhotovoltaikService) {
+    this.froniusService.logGeneralInfo()
+  }
 
-    @Get('/ping')
-    ping(): string{
-        return "pong"
-    }
+  @Get('/ping')
+  ping(): string {
+    return "pong"
+  }
 
-    @Get()
-    generalInfo(): Promise<{}>{
-        return this.froniusService.generalInfo()
-    }
+  @Get()
+  generalInfo(): Promise<Record<string, unknown>> {
+    return this.froniusService.generalInfo()
+  }
 
-    @Get("/data")
-    information(): Promise<{}>{
-        return this.froniusService.data()
-    }
+  @Get("/data")
+  information(): Promise<Record<string, unknown>> {
+    return this.froniusService.data()
+  }
 
-    @Get('/overgeneration')
-    uebergenerierung(): Promise<number>{
-        return this.froniusService.uebergenerierung()
-    }
+  @Get('/overgeneration')
+  uebergenerierung(): Promise<number | null> {
+    return this.froniusService.uebergenerierung()
+  }
 
-    @Get('/healthcheck')
-    healthCheck(): Promise<boolean>{
-        return this.froniusService.healthCheck()
-    }
+  @Get('/healthcheck')
+  healthCheck(): Promise<boolean> {
+    return this.froniusService.healthCheck()
+  }
 }
